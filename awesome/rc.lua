612771e9842acfef26bb7ae200bf291b5c77eff4
Miranda de Oliveira Thibault <thibault.mirandadeoliveira@epitech.eu>
2015-11-02 11:11:31 +0100

    new keybindings

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -62,18 +62,16 @@ local shexec = awful.util.spawn_with_shell
 modkey        = "Mod4"
 terminal      = "urxvt"
 browser       = "firefox"
-filemanager   = "spacefm"
 
 -- | Table of layouts | --
 
 local layouts =
 {
    lain.layout.uselesstile,
-   lain.layout.centerfair,
-   lain.layout.centerwork,
    lain.layout.uselessfair,
-   lain.layout.cascade,
-   lain.layout.uselesspiral
+   lain.layout.centerfair,
+   lain.layout.uselesspiral,
+   lain.layout.centerwork
 }
 
 lain.layout.centerfair.nmaster = 3
@@ -92,7 +90,7 @@ end
 
 tags = {}
 for s = 1, screen.count() do
-    tags[s] = awful.tag({ "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " }, s, layouts[1])
+    tags[s] = awful.tag({ "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, s, layouts[1])
 end
 
 -- | Menu | --
@@ -454,9 +452,11 @@ root.buttons(awful.util.table.join(
 -- | Key binding | --
 
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
-    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+    awful.key({ modkey,           }, "j",   awful.tag.viewprev       ),
+    awful.key({ modkey,           }, "k",  awful.tag.viewnext       ),
+    --awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
+    --awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
+    awful.key({ modkey,           }, "Tab", awful.tag.history.restore),
 
     awful.key({ modkey,	"Control" }, "Up", function ()
     		 awful.util.spawn("amixer set Master 1%+")
@@ -467,51 +467,50 @@ globalkeys = awful.util.table.join(
     		 volumewidget.update()
     					     end),
 
-    awful.key({ modkey, "Shift" }, "Right",
+    awful.key({ modkey, }, "l",
         function ()
             awful.client.focus.byidx( 1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey, "Shift"  }, "Left",
+    awful.key({ modkey, }, "h",
         function ()
             awful.client.focus.byidx(-1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show() end),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+    awful.key({ modkey, "Shift"   }, "l", function () awful.client.swap.byidx(  1)    end),
+    awful.key({ modkey, "Shift"   }, "h", function () awful.client.swap.byidx( -1)    end),
+    awful.key({ modkey, "Control" }, "Right", function () awful.screen.focus_relative( 1) end),
+    awful.key({ modkey, "Control" }, "Left", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
-    awful.key({ modkey,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end),
+--    awful.key({ modkey,           }, "Tab",
+--        function ()
+--            awful.client.focus.history.previous()
+--            if client.focus then
+--                client.focus:raise()
+--            end
+--        end),
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+    awful.key({ modkey, "Shift"   }, "k",     function () awful.tag.incmwfact( 0.05)    end),
+    awful.key({ modkey, "Shift"   }, "j",     function () awful.tag.incmwfact(-0.05)    end),
+    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incnmaster( 1)      end),
+    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incnmaster(-1)      end),
+    awful.key({ modkey, "Control" }, "[",     function () awful.tag.incncol( 1)         end),
+    awful.key({ modkey, "Control" }, "]",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
     awful.key({ modkey,           }, "F12",   function () awful.util.spawn("xscreensaver-command -lock") end),
 
     -- Pomodoro
 
-    awful.key({ modkey,         }, ":",  function() pomodoro:start() end),
-    awful.key({ modkey,         }, "!",  function() pomodoro:stop() end),
+    awful.key({ modkey,         }, ";",  function() pomodoro:start() end),
+    awful.key({ modkey,         }, "'",  function() pomodoro:stop() end),
 
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
@@ -521,7 +520,8 @@ globalkeys = awful.util.table.join(
     awful.key({                   }, "XF86MonBrightnessDown", function () awful.util.spawn("xbacklight -5") end),
     awful.key({                   }, "XF86AudioMute", function () awful.util.spawn("pulseaudio-ctl mute") end),
     awful.key({                   }, "XF86Sleep", function () awful.util.spawn("systemctl suspend") end),
-    awful.key({                   }, "XF86Display", function () awful.util.spawn("~/.bin/sdisp.sh") end),
+    awful.key({                   }, "XF86AudioLowerVolume", function () awful.util.spawn("amixer set Master 10%-") end),
+    awful.key({                   }, "XF86AudioRaiseVolume", function () awful.util.spawn("amixer set Master 10%+") end),
     awful.key({                   }, "Print", function () awful.util.spawn("scrot -e 'mv $f ~/Images/shots/'") end),
 
     -- Prompt
